55583f30045c1d666a58ca7a1c5f166a400413d9
Ignatius <109012779+Ignatius47@users.noreply.github.com>
2024-12-05 15:28:10 +0300

    Delete fare directory

--- fare/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FareConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'fare'
--- fare/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Fare
-
-class FareSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Fare
-        fields = '__all__'
